a8b10ca2133f6e94c116499dde641a49fb97cca8
vingiarrusso <vin.giarrusso@gmail.com>
2019-02-16 15:58:12 -0800

    refactor last solution

--- practice_problems/convert_sorted_array_to_bst.py
+++ practice_problems/convert_sorted_array_to_bst.py
@@ -3,14 +3,12 @@ leetcode 108
 """
 
 def convert_sorted_array_to_bst(nums):
-    def make_tree(nums, root=None):
-        if not len(nums):
-            return root
+    if not len(nums):
+        return None 
 
-        half = len(nums) // 2
-        root = TreeNode(nums[half])
-        root.left = make_tree(nums[:half])
-        root.right = make_tree(nums[half+1:])
-        return root
+    half = len(nums) // 2
+    root = TreeNode(nums[half])
+    root.left = convert_sorted_array_to_bst(nums[:half])
+    root.right = convert_sorted_array_to_bst(nums[half+1:])
 
-    return make_tree(nums)
+    return root
